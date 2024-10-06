b8341bde8220008cdfda04ff0ddd60556a82126e
abhayybhatt <147072724+abhayybhatt@users.noreply.github.com>
2024-10-06 12:36:26 +0530

    Add files via upload

---
+++ Simon Says Game/app.js
@@ -0,0 +1,84 @@
+let userSeq=[];
+let gameSeq=[];
+let highestScore = 0;
+
+let btns = ["yellow","red","purple","green"];
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector('h2');
+let highestScoreDisplay = document.querySelector('#highest-score');
+document.addEventListener('keypress',  function(){
+    if(started==false){
+        console.log('game started');
+        started= true;
+        levelUp();
+    }
+});
+
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+        }, 250);
+}
+function userFlash(btn){
+    btn.classList.add("userFlash");
+    setTimeout(function(){
+        btn.classList.remove("userFlash");
+        }, 250);
+}
+function levelUp(){
+    userSeq = []; //reset kr rahe hai agr level up hogi to, jisse ki start se pura pattern follow krna hoga.
+    level++;
+    h2.innerText= `Level ${level}`;
+
+    //random btn choose
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    // console.log(randIdx);
+    // console.log(randColor);
+    // console.log(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq); 
+    gameFlash(randBtn);
+}
+function checkAns(idx){
+    // console.log('curr level: ', level);
+    if(userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1000);
+        }
+    }else{
+        let currentScore = level;
+        h2.innerHTML= `Game Over! Your score was <b> ${level} <b> <br>Press any key to start.`;
+        document.querySelector('body').style.backgroundColor = 'red';
+        setTimeout(function(){
+            document.querySelector('body').style.backgroundColor = 'white';
+        }, 150);
+        reset();
+        if(currentScore > highestScore){
+            highestScore = currentScore;
+            highestScoreDisplay.innerText = `Highest Score: ${highestScore}`;
+        }
+    }
+}
+function btnPress(){
+    let btn = this;
+    userFlash(btn);
+    userColor = btn.getAttribute('id');
+    console.log(userColor);
+    userSeq.push(userColor);
+    checkAns(userSeq.length - 1);
+}
+let allBtns = document.querySelectorAll('.btn');
+for(btn of allBtns){
+    btn.addEventListener('click', btnPress);
+}
+function reset(){
+    started = false;
+    gameSeq=[];
+    userSeq=[]
+    level = 0;
+}
